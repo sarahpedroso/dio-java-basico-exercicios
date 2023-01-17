6b13924069c05bf356f10dbe5ae91795d8a09202
Sarah Pedroso <sarahcampinho@hotmail.com>
2023-01-17 19:58:54 -0300

    exercicio 2

---
+++ primeiro-projeto-java/src/exercicio2.java
@@ -0,0 +1,13 @@
+import javax.lang.model.util.ElementScanner6;
+
+public class exercicio2 {
+    public static void main (String [] args) {
+        int mediaFinal = 10;
+        if (mediaFinal <6 )
+        System.out.println ("Reprovado");
+        else if (mediaFinal ==6 )
+        System.out.println ("Prova Minerva");
+        else 
+        System.out.print ("Aprovado");
+    }
+}
